c76a85cf62068d79dcbf73a2748952e90a8c7024
Jacob <jacobfisher311@gmail.com>
2023-02-10 13:22:06 -0800

    minor refactoring to allow button press changes

--- choropleth.py
+++ choropleth.py
@@ -6,13 +6,20 @@ from plotly.subplots import make_subplots
 GEOJSON_FILE = "USStates.geojson"
 df = pd.read_csv("taxData2021.csv")
 
-fig = make_subplots(rows=1, cols=2,subplot_titles=['title1','title2'],
+# Should change on button press
+# 1 is left, 2 is right
+title1 = 'title1'
+title2 = 'title2'
+view1 = 'T20'
+view2 = 'T01'
+
+fig = make_subplots(rows=1, cols=2,subplot_titles=[title1,title2],
                     specs=[[{'type':'choropleth'},{'type':'choropleth'}]])
 fig.add_trace(
-    go.Choropleth(locations=df['item'], z=df['T20'], locationmode='USA-states', colorbar=dict(thickness=20, x=0.46) ), row=1, col=1)
+    go.Choropleth(locations=df['item'], z=df[view1], locationmode='USA-states', colorbar=dict(thickness=20, x=0.46) ), row=1, col=1)
 
 fig.add_trace(
-    go.Choropleth(locations=df['item'], z=df['T01'], locationmode='USA-states', colorbar=dict(thickness=20, x=1.02)), row=1, col=2)
+    go.Choropleth(locations=df['item'], z=df[view2], locationmode='USA-states', colorbar=dict(thickness=20, x=1.02)), row=1, col=2)
 
 fig.update_geos(
     visible=False, resolution=110, scope="usa",
