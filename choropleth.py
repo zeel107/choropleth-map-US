c5cab98ee75d48d10c554097c84230894202a296
Aidan Fallstrom <57118488+zeel107@users.noreply.github.com>
2023-02-12 12:35:37 -0800

    better initial image sizing
    The initial image should always fit on the screen.  I hope...

--- choropleth.py
+++ choropleth.py
@@ -6,9 +6,9 @@ from PIL import Image, ImageTk
 from functools import partial
 
 image_width = 1200
-image_height = 600
+image_height = 500
 window_geometry = '1400x600'
-image_file_name = 'chloroplath_map.png'
+image_file_name = 'choropleth_map.png'
 button_text = ['T10', 'T12', 'T13', 'T16', 'T19', 'T23', 'T24', 'T25']
 title_text = ['Alcoholic Beverages Sales Tax', 'Insurance Premiums Sales Tax', 'Motor Fuels Sales Tax',
               'Tobacco Products Sales Tax', 'Other Selective Sales and Gross Receipts Taxes',
@@ -22,10 +22,9 @@ class App():
     def __init__(self):
 
         self.root = tk.Tk()
-        self.root.title("Chloroplath map")
-        self.root.geometry(window_geometry)
-        self.map_image = Image.open(image_file_name)
-        self.map_photo = ImageTk.PhotoImage(self.map_image)
+        w, h = self.root.winfo_screenwidth() - 100, self.root.winfo_screenheight() - 100
+        self.root.geometry("%dx%d+0+0" % (w, h))
+        self.root.title("Choropleth map")
 
         self.button_list = []
 
@@ -42,10 +41,37 @@ class App():
             self.button_list.append(tk.Button(self.right_buttons, text=button_text[i], command=partial(self.change_vis, i)))
             self.button_list[i].pack(side=tk.LEFT, pady = 3, padx = 3)
 
-        self.cv = tk.Canvas(width=image_width, height=image_height)
+        self.image_width = w- self.left_buttons.winfo_width() - self.right_buttons.winfo_width() - 10
+        self.image_height = h-10
+
+        self.fig = make_subplots(rows=1, cols=2, subplot_titles=[current_titles[0], current_titles[1]],
+                            specs=[[{'type': 'choropleth'}, {'type': 'choropleth'}]])
+        self.fig.add_trace(
+            go.Choropleth(locations=df['item'], z=df[current_views[0]], locationmode='USA-states',
+                          colorbar=dict(thickness=20, x=0.46)), row=1, col=1)
+
+        self.fig.add_trace(
+            go.Choropleth(locations=df['item'], z=df[current_views[1]], locationmode='USA-states',
+                          colorbar=dict(thickness=20, x=1.02)), row=1, col=2)
+
+        self.fig.update_geos(
+            visible=False, resolution=110, scope="usa",
+            showcountries=True, countrycolor="Black"
+        )
+        self.fig.update_layout(width=self.image_width,
+                               height=self.image_height, margin={"r": 0, "t": 100, "l": 0, "b": 0})
+        self.fig.write_image(image_file_name)
+
+        self.map_image = Image.open(image_file_name)
+        self.map_photo = ImageTk.PhotoImage(self.map_image)
+
+
+
+        self.cv = tk.Canvas(width=self.image_width, height=self.image_height)
         self.cv.create_image(0, 0, image=self.map_photo, anchor='nw')
         self.cv.pack(anchor=tk.S)
         self.root.mainloop()
+
 
     def change_vis(self, button_num):
         print(button_num)
@@ -56,47 +82,32 @@ class App():
             current_views[0] = views[button_num]
             current_titles[0] = title_text[button_num]
 
-        fig = make_subplots(rows=1, cols=2, subplot_titles=[current_titles[0], current_titles[1]],
+        self.fig = make_subplots(rows=1, cols=2, subplot_titles=[current_titles[0], current_titles[1]],
                             specs=[[{'type': 'choropleth'}, {'type': 'choropleth'}]])
-        fig.add_trace(
+        self.fig.add_trace(
             go.Choropleth(locations=df['item'], z=df[current_views[0]], locationmode='USA-states',
                           colorbar=dict(thickness=20, x=0.46)), row=1, col=1)
 
-        fig.add_trace(
+        self.fig.add_trace(
             go.Choropleth(locations=df['item'], z=df[current_views[1]], locationmode='USA-states',
                           colorbar=dict(thickness=20, x=1.02)), row=1, col=2)
 
-        fig.update_geos(
+        self.fig.update_geos(
             visible=False, resolution=110, scope="usa",
             showcountries=True, countrycolor="Black"
         )
-        image_width = self.root.winfo_width() - self.left_buttons.winfo_width() - self.right_buttons.winfo_width() - 10
-        image_height = self.root.winfo_height() - 10
-        fig.update_layout(width=image_width, height=image_height, margin={"r": 0, "t": 100, "l": 0, "b": 0})
-        fig.write_image(image_file_name)
+        self.image_width = self.root.winfo_width() - self.left_buttons.winfo_width() - self.right_buttons.winfo_width() - 10
+        self.image_height = self.root.winfo_height() - 10
+        self.fig.update_layout(width=self.image_width, height=self.image_height, margin={"r": 0, "t": 100, "l": 0, "b": 0})
+        self.fig.write_image(image_file_name)
         self.map_image = Image.open(image_file_name)
         self.map_photo = ImageTk.PhotoImage(self.map_image)
         self.cv.destroy()
-        self.cv = tk.Canvas(width=image_width, height=image_height)
+        self.cv = tk.Canvas(width=self.image_width, height=self.image_height)
         self.cv.create_image(0, 0, image=self.map_photo, anchor='nw')
         self.cv.pack(anchor=tk.S)
 
 GEOJSON_FILE = "USStates.geojson"
 df = pd.read_csv("normalizedTaxData.csv")
-
-fig = make_subplots(rows=1, cols=2,subplot_titles=[current_titles[0],current_titles[1]],
-                    specs=[[{'type':'choropleth'},{'type':'choropleth'}]])
-fig.add_trace(
-    go.Choropleth(locations=df['item'], z=df[current_views[0]], locationmode='USA-states', colorbar=dict(thickness=20, x=0.46) ), row=1, col=1)
-
-fig.add_trace(
-    go.Choropleth(locations=df['item'], z=df[current_views[1]], locationmode='USA-states', colorbar=dict(thickness=20, x=1.02)), row=1, col=2)
-
-fig.update_geos(
-    visible=False, resolution=110, scope="usa",
-    showcountries=True, countrycolor="Black"
-)
-fig.update_layout(width=image_width, height=image_height, margin={"r":0,"t":100,"l":0,"b":0})
-fig.write_image(image_file_name)
 
 myapp = App()
